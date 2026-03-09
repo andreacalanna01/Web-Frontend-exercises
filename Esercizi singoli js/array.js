b43a4cdd144dc084c5cbcd42ef16c0e9c83a6b0c
Andrea Calanna <andreacalanna01@gmail.com>
2026-03-09 16:29:03 +0100

    Delete Esercizi singoli js directory

--- Esercizi singoli js/array.js
+++
@@ -1,6 +0,0 @@
-var array = [10,22,33,4,67,89];
-
-for (var i=0; i<array.length; i++)
-console.log (array[i]);
-
-console.log(array[10])
--- Esercizi singoli js/foreach.js
+++
@@ -1,6 +0,0 @@
-var array = [10,22,33,4,67,89];
- array.forEach(element => {
-    console.log(element)
- });
-
- 
